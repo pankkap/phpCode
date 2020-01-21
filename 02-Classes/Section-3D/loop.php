edbc4dfbedc743a823c1e3da803954b224a61ee5
Pankaj kapoor <pankkap@gmail.com>
2020-01-21 11:56:37 +0530

    section-d 21-1-2020

---
+++ 02-Classes/Section-3D/loop.php
@@ -0,0 +1,19 @@
+<?php
+
+// Loop control structure
+/*
+1. while loop
+2. do while loop
+3. for loop
+4. foreach loop
+*/
+
+$a;
+for($a=1;$a<11;$a++)
+echo "$a<br>";
+
+
+
+
+
+?>
---
+++ 10-Examples/foreach.php
@@ -0,0 +1,11 @@
+<?php
+
+$animals_list = array("Lion","Wolf","Dog","Leopard","Tiger");
+
+foreach($animals_list as $array_values){
+
+echo $array_values . "<br>";
+
+}
+
+?>
