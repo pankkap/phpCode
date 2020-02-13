070eeef7747bebcb53aba75450560bc4816fd276
pankkap <pankkap@gmail.com>
2020-02-13 15:44:27 +0530

    Insertion complete for section c

--- 02-Classes/Section-3C/05-Database/insertFirstRecord.php
+++ 02-Classes/Section-3C/05-Database/insertFirstRecord.php
@@ -1,28 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+    <label for="">RollNo</label>
+    <input type="text" name="t1"><br><br>
+    <label for="">Name</label>
+    <input type="text" name="t2"><br><br>
+    <label for="">Class</label>
+    <input type="text" name="t3"><br><br>
+    <input type="submit" value="submit" name="b1"><br><br>
+    </form>
+</body>
+</html>
+
 <?php
 
-include 'connection.php';
-
-// $query = "INSERT INTO `secctable`(`rollno`, `name`, `class`) VALUES (104,'manoj','3C')";
-
-$query = "INSERT INTO secctable1 VALUES (105,'Sandeep','3A')";
-
-$data = mysqli_query($conn,$query);
-
-if($data)
+if(isset($_POST['b1']))
 {
-    echo "Data inserted in Database";
+    $roll = $_POST['t1'];
+    $name = $_POST['t2'];
+    $class = $_POST['t3'];
+    
+    if($roll!="" and $name!="" && $class!="")
+    {
+
+        include 'connection.php';
+
+        // $query = "INSERT INTO `secctable`(`rollno`, `name`, `class`) VALUES (104,'manoj','3C')";
+
+        $query = "INSERT INTO secctable VALUES ('$roll','$name','$class')";
+
+        $data = mysqli_query($conn,$query);
+
+        if($data)
+        {
+            echo "Data inserted in Database";
+        }
+        else
+        {
+            echo "Record not Inserted";
+        }
+
+    }
+    else
+    {
+        echo "<p style='color:red'>All Fields are Required</p>";
+    }
+
 }
-else
-{
-    echo "Record not Inserted";
-}
-
-
-
-
-
-
-
-
 
 ?>
