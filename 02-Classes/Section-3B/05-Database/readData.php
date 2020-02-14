cebe8b8914a1a7088b0d83964fc5931afb3926f8
pankkap <pankkap@gmail.com>
2020-02-14 14:53:15 +0530

    table bootstrap

--- 02-Classes/Section-3B/05-Database/readData.php
+++ 02-Classes/Section-3B/05-Database/readData.php
@@ -1,9 +1,16 @@
-<style>
-td
-{
-    padding:10px;
-}
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" ">
+</head>
+<body>
+    
+</body>
+</html>
 
 
 
@@ -20,14 +27,15 @@ $totRec = mysqli_num_rows($data);
 if($totRec!=0)
 {
     ?>
-
-    <table border=1>
+<div class="container">
+<table class="table">
+  <thead class="thead-dark">
         <tr>
             <th>Rollno</th>
             <th>Name</th>
             <th>Class</th>
         </tr>
-    
+</thead>
 
     <?php
 
@@ -58,3 +66,4 @@ else
 
 
 
+</div>
